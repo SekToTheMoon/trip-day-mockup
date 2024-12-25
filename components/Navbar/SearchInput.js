7cd035adae64b242a8daadb1f0fe2893f0dce927
oakkharaphong <oakkharaphong1@gmail.com>
2024-12-26 01:30:58 +0700

    deploy now

--- components/Navbar/SearchInput.js
+++ components/Navbar/SearchInput.js
@@ -1,32 +1,32 @@
-"use client";
+// "use client";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Input } from "../ui/input";
 import { useEffect, useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
 import { Search } from "lucide-react";
 const SearchInput = () => {
-  const searchURL = useSearchParams();
-  const url = usePathname();
-  const searchParams = searchURL.get("search");
-  const { replace } = useRouter();
-  const [search, setSearch] = useState(searchParams?.toString() || "");
+  // const searchURL = useSearchParams();
+  // const url = usePathname();
+  // const searchParams = searchURL.get("search");
+  // const { replace } = useRouter();
+  // const [search, setSearch] = useState(searchParams?.toString() || "");
 
-  const handleSearch = useDebouncedCallback((value) => {
-    const params = new URLSearchParams(searchURL);
-    if (value) {
-      params.set("search", value);
-    } else {
-      params.delete("search");
-    }
-    replace(`${url}?${params.toString()}`);
-  }, 500);
+  // const handleSearch = useDebouncedCallback((value) => {
+  //   const params = new URLSearchParams(searchURL);
+  //   if (value) {
+  //     params.set("search", value);
+  //   } else {
+  //     params.delete("search");
+  //   }
+  //   replace(`${url}?${params.toString()}`);
+  // }, 500);
 
-  useEffect(() => {
-    // code body
-    if (!searchParams) {
-      setSearch("");
-    }
-  }, [searchParams]);
+  // useEffect(() => {
+  //   // code body
+  //   if (!searchParams) {
+  //     setSearch("");
+  //   }
+  // }, [searchParams]);
 
   return (
     <div className="relative flex-1">
@@ -37,11 +37,11 @@ const SearchInput = () => {
         type="text"
         placeholder="Destination, hotel, flight"
         className="max-w-md "
-        onChange={(e) => {
-          setSearch(e.target.value);
-          handleSearch(e.target.value);
-        }}
-        value={search}
+        // onChange={(e) => {
+        //   setSearch(e.target.value);
+        //   handleSearch(e.target.value);
+        // }}
+        // value={search}
       />
     </div>
   );
